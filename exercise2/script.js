6a85404c4f7b9a2d99635e6a2ddd4d620c0701c8
Kyle Maune <kmaune71@gmail.com>
2014-09-30 11:45:49 -0700

    added jQuery image slider

--- exercise2/script.js
+++ exercise2/script.js
@@ -1,9 +1,33 @@
-$(function() {
-	var imgs = [
-		"images/SooAdorable.jpg",
-		"images/OMGcute.jpg",
-		"images/PreciousLittlePuppyIWillStealYou.jpg",
-		"images/awwww.jpg",
-		"images/Meh.jpg"
-	];
-});
+// $(document).ready() prevents script from running until document is fully loaded
+  $(document).ready(function() {
+    var $slider = $('.slider');
+    var $slide = 'li'; // could also use 'img' if you're not using a ul
+    var $transition_time = 10;
+    var $time_between_slides = 3000; // 3 seconds
+
+    function slides(){
+      return $slider.find($slide);
+    }
+
+    slides().fadeOut();
+
+    // set active classes
+    slides().first().addClass('active');
+    slides().first().fadeIn($transition_time);
+
+    // auto scroll
+    $interval = setInterval(
+      function(){
+        var $i = $slider.find($slide + '.active').index();
+
+        slides().eq($i).removeClass('active');
+        slides().eq($i).fadeOut($transition_time);
+
+        if (slides().length == $i + 1) $i = -1; // loop to start
+
+        slides().eq($i + 1).fadeIn($transition_time);
+        slides().eq($i + 1).addClass('active');
+      }
+      , $transition_time +  $time_between_slides
+    );
+  });
